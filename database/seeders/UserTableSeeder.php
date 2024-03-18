ad1490e9aa92050e4e8ee9d7ab582fa215409fa1
nopalpadail18 <nopalpadail@gmail.com>
2024-03-18 15:10:36 +0800

    laravel React

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin =User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className='text-center mt-[300px]'>
+                Aw3okokoakowk
+            </div>
+        </>
+    );
+}
